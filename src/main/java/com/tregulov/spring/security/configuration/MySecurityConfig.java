58881e4bf32d6c9cb52c18156645a550c351fc4e
WindzoR <windzr67@gmail.com>
2021-12-19 14:30:18 +0300

    Transfer users & passwords to database and encoder by {bcrypt}

--- src/main/java/com/tregulov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/tregulov/spring/security/configuration/MySecurityConfig.java
@@ -1,22 +1,22 @@
 package com.tregulov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("user").password("user").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("hr").password("hr").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
